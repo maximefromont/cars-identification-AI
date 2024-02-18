8f971cd1cd1c11b51101e6f71988790cf8f0bd67
LEBORGNE Kévin <leborgne.kevin@universite-paris-saclay.fr>
2024-02-18 13:06:37 +0100

    add last- model

--- data-stats/dataset-stats.py
+++ data-stats/dataset-stats.py
@@ -59,10 +59,43 @@ def get_number_of_car_model(root_folder="sorted-dataset"):
         for key in car_model_counts.keys():
             f.write("%s,%s\n"%(key,car_model_counts[key]))
     return car_model_counts
-    
 
-brand_date = get_number_of_brand()
-model_data=get_number_of_car_model()
+#get the number of brand by counting the number of folder in brand-sorted-dataset and the number of for each brand
+def get_number_of_model_per_brand(root_folder="complete-car-dataset"):
+    # Initialize an empty dictionary to store counts per brand
+    brand_counts = {}
+
+    # Iterate through subfolders
+    for brand_folder in os.listdir(root_folder):
+        brand_path = os.path.join(root_folder, brand_folder)
+
+        # Check if the item in the root folder is a directory
+        if os.path.isdir(brand_path):
+            # Count the number of subfolder in the brand subfolder
+            num_images = len([f for f in os.listdir(brand_path) if os.path.isdir(os.path.join(brand_path, f))])
+            
+
+            # Store the count in the dictionary
+            brand_counts[brand_folder] = num_images
+
+    # Print the counts per brand
+    for brand, count in brand_counts.items():
+        print(f"{brand}: {count} images")
+
+    #sort the dictionary by value
+    brand_counts = dict(sorted(brand_counts.items(), key=lambda item: item[1], reverse=True))
+    #save this information to csv file   
+    with open('stat-'+'model_per_brand'+'.csv', 'w') as f:
+        for key in brand_counts.keys():
+            f.write("%s,%s\n"%(key,brand_counts[key]))
+
+    return brand_counts
+
+
+
+# brand_date = get_number_of_brand()
+# model_data=get_number_of_car_model()
+get_number_of_model_per_brand()
 
 
 
